6c68fdd70fa63027d11a058fa3ef2b45b9437bd0
Jashvanth Reddy <75676249+JashvanthReddy@users.noreply.github.com>
2021-03-19 16:15:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video= createCapture(VIDEO);
+    video.hide();
+    classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/STFzGV5Xz/', modeloaded);
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);   
+}
+
+function gotResult(error, results){
+    if(error){
+        ole.log(error);
+    }
+    else{
+        console.log(results); 
+        document.getElementById("result_object_name").innerHTML= results[0].label;
+    }
+}
+
+function modeloaded(){
+    console.log(modeloaded)
+}
